60900b5430347248151da720800162eee9b3551f
Your Name <you@example.com>
2026-06-13 20:19:01 +0600

    tittle ubdate

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -9,22 +9,19 @@ const AdminRoute = ({ children }) => {
     const { role, roleLoading } = useUserRole();
     const location = useLocation();
 
-    // ১. অথেন্টিকেশন চেক এবং রোল ফেচিং চেক
+
     if (authLoading || roleLoading) {
         return <LoadingSpinner />;
     }
 
-    // ২. লগইন না থাকলে লগইন পেজে পাঠান
+
     if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
 
-    // ৩. শুধুমাত্র admin এবং developer এক্সেস পাবে
     if (role === 'admin' || role === 'developer') {
         return children;
     }
-
-    // ৪. অন্য সাধারণ ইউজার হলে তাকে ড্যাশবোর্ডের মূল পেজে বা হোম পেজে রিডাইরেক্ট করুন
     return <Navigate to="/dashboard" replace />;
 };
 
